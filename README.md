a96acebd9d93c62ad7eae554d6d783e2e451a382
Oleg991 <o.n.eremenko@gmail.com>
2023-01-03 22:36:59 +0300

    Minor updates
    - Added an Example app
    - Updated README file
    - Refactor

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ImagePicker
 
-A PHPickerViewController wrapped inside a UIViewControllerRepresentable to be used in SwiftUI views to pick images from PhotoLibrary.
+A PHPickerViewController wrapped in a UIViewControllerRepresentable to be used in SwiftUI views to pick images from PhotoLibrary.
+
+Currently PHPickerViewController has a bug: it allows picking multiple items by tapping multiple times very quickly (even the same item) before the picker disappears, even with selectionLimit set to 1.
--- Sources/ImagePicker/ImagePicker.swift
+++ Sources/ImagePicker/ImagePicker.swift
@@ -1,12 +1,17 @@
 import SwiftUI
 import PhotosUI
 
+/// A `PHPickerViewController` wrapped inside a `UIViewControllerRepresentable`
+///
+/// The picker will be automatically dismissed when the user
+/// - completes a selection using the `Add` button (or by tapping a single photo with `selectionLimit` set to `1`)
+/// - dismisses `PHPickerViewController` using the `Cancel` button
 public struct ImagePicker: UIViewControllerRepresentable {
     @Binding private var pickedImages: [UIImage]
-    @Binding private var isPresented: Bool
-    /// The maximum number of assets that can be selected. Default is 1.
+    /// The maximum number of photos that can be selected. Default is 1.
     ///
-    /// Setting `selectionLimit` to 0 means maximum supported by the system.
+    /// - Setting `selectionLimit` to 0 means maximum supported by the system.
+    /// - The more photos the user selects, the longer it will take the system to get them in your app.
     private var selectionLimit = 1
     /// The quality of the resulting JPEG image, expressed as a value from `0.0` to `1.0`.
     ///
@@ -16,31 +21,26 @@ public struct ImagePicker: UIViewControllerRepresentable {
 
     public init(
         pickedImages: Binding<[UIImage]>,
-        isPresented: Binding<Bool>,
         selectionLimit: Int = 1,
         compressionQuality: CGFloat? = nil
     ) {
         self._pickedImages = pickedImages
-        self._isPresented = isPresented
         self.selectionLimit = selectionLimit
         self.compressionQuality = compressionQuality
     }
 
-    public func makeUIViewController(context: Context) -> some UIViewController {
+    public func makeUIViewController(context: Context) -> PHPickerViewController {
         var configuration = PHPickerConfiguration(photoLibrary: .shared())
         configuration.filter = .images
         configuration.selectionLimit = selectionLimit
-
-        let photoPickerViewController = PHPickerViewController(configuration: configuration)
-        photoPickerViewController.delegate = context.coordinator
-        return photoPickerViewController
+        let viewController = PHPickerViewController(configuration: configuration)
+        viewController.delegate = context.coordinator
+        return viewController
     }
 
-    public func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+    public func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
 
-    public func makeCoordinator() -> Coordinator {
-        .init(self)
-    }
+    public func makeCoordinator() -> Coordinator { .init(self) }
 
     final public class Coordinator: PHPickerViewControllerDelegate {
         private let parent: ImagePicker
@@ -50,13 +50,15 @@ public struct ImagePicker: UIViewControllerRepresentable {
         }
 
         public func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-            let queue = DispatchQueue(label: "ImagePickerConcurrentQueue", qos: .userInteractive, attributes: .concurrent)
+            let imageItems = results
+                .map { $0.itemProvider }
+                .filter { $0.canLoadObject(ofClass: UIImage.self) }
             let group = DispatchGroup()
-            for image in results {
-                guard image.itemProvider.canLoadObject(ofClass: UIImage.self) else { continue }
+            var finalArray = [UIImage]()
+            for item in imageItems {
                 group.enter()
-                image.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] newImage, _ in
-                    if let image = newImage as? UIImage {
+                item.loadObject(ofClass: UIImage.self) { [weak self] element, _ in
+                    if let image = element as? UIImage {
                         let finalImage: UIImage
                         if let compressionQuality = self?.parent.compressionQuality,
                            let compressedData = image.jpegData(compressionQuality: compressionQuality),
@@ -65,15 +67,14 @@ public struct ImagePicker: UIViewControllerRepresentable {
                         } else {
                             finalImage = image
                         }
-                        queue.async(flags: .barrier) {
-                            self?.parent.pickedImages.append(finalImage)
-                        }
+                        finalArray.append(finalImage)
                     }
                     group.leave()
                 }
             }
             group.notify(queue: .main) { [weak self] in
-                self?.parent.isPresented = false
+                self?.parent.pickedImages.append(contentsOf: finalArray)
+                picker.dismiss(animated: true)
             }
         }
     }
