160f4183740c95095ffb96aac316b64824fa779c
Oleg991 <o.n.eremenko@gmail.com>
2023-01-03 21:02:39 +0300

    Added compressionQuality

--- Sources/ImagePicker/ImagePicker.swift
+++ Sources/ImagePicker/ImagePicker.swift
@@ -8,15 +8,22 @@ public struct ImagePicker: UIViewControllerRepresentable {
     ///
     /// Setting `selectionLimit` to 0 means maximum supported by the system.
     private var selectionLimit = 1
+    /// The quality of the resulting JPEG image, expressed as a value from `0.0` to `1.0`.
+    ///
+    /// - The value `0.0` represents the maximum compression (or lowest quality) while the value `1.0` represents the least compression (or best quality).
+    /// - If not specified, no compression will be applied.
+    private let compressionQuality: CGFloat?
 
     public init(
         pickedImages: Binding<[UIImage]>,
         isPresented: Binding<Bool>,
-        selectionLimit: Int = 1
+        selectionLimit: Int = 1,
+        compressionQuality: CGFloat? = nil
     ) {
         self._pickedImages = pickedImages
         self._isPresented = isPresented
         self.selectionLimit = selectionLimit
+        self.compressionQuality = compressionQuality
     }
 
     public func makeUIViewController(context: Context) -> some UIViewController {
@@ -50,8 +57,16 @@ public struct ImagePicker: UIViewControllerRepresentable {
                 group.enter()
                 image.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] newImage, _ in
                     if let image = newImage as? UIImage {
+                        let finalImage: UIImage
+                        if let compressionQuality = self?.parent.compressionQuality,
+                           let compressedData = image.jpegData(compressionQuality: compressionQuality),
+                           let compressedImage = UIImage(data: compressedData) {
+                            finalImage = compressedImage
+                        } else {
+                            finalImage = image
+                        }
                         queue.async(flags: .barrier) {
-                            self?.parent.pickedImages.append(image)
+                            self?.parent.pickedImages.append(finalImage)
                         }
                     }
                     group.leave()
